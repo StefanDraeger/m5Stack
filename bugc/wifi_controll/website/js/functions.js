c83d637f2344fb9e7d8e75b447de0e63416f0928
Stefan Draeger <stefandraeger@gmail.com>
2022-02-28 14:20:39 +0100

    Webseite aktualisiert

--- bugc/wifi_controll/website/js/functions.js
+++ bugc/wifi_controll/website/js/functions.js
@@ -1,5 +1,21 @@
 $( document ).ready(function() {
-	$("buton").click(function(this) {
-		alert(this.id);
+	console.log("ready");
+	$("button").click(function() {
+		var direction = "";
+		switch ($(this).attr("id")){
+			case "topBtn": direction = "forward"; break;
+			case "bottomBtn": direction = "backward"; break;
+			case "leftBtn": direction = "left"; break;
+			case "rightBtn": direction = "right"; break;
+			case "stopBtn": direction = "stop"; break;
+		}
+		console.log("move BugC to: " + direction);
+		moveBugC(direction);
 	});
 });
+
+function moveBugC(direction){
+	var url = "http://"+ipAdresse+"/move?direction=" + direction;
+	console.log("call: " + url);
+	$.get(url);
+}
